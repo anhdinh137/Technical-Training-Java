844d35ca7efc4d3608fb978fdfbeef3b4f1d683a
anhdinh137 <anh.dinh@hitachids.com>
2025-10-16 14:01:28 +0700

    TTJ-13 type casting final

--- Day1JavaBasics/3.TypeCasting/typeCasting.java
+++ Day1JavaBasics/3.TypeCasting/typeCasting.java
@@ -8,5 +8,8 @@ public class typeCasting {
         System.out.println("Num 1:" + num1 + ", Num 2: "+ num2);
 
         // Narrowing Casting(Has to be done manually): double -> float -> long -> int -> char -> short -> byte
+        double num3 = 0;
+        int num4 = (int) num3;
+        System.out.println("Num 3:" + num3 + ", Num 4: "+ num4);
     }
 }
