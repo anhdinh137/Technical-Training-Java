a903b9d67f087b9bc8621dfd9e82923318f7226e
anhdinh137 <anh.dinh@hitachids.com>
2025-10-20 14:15:11 +0700

    TTJ-42 static class

---
+++ Day3OOPinJava/2. StaticClass/staticClass.java
@@ -0,0 +1,10 @@
+public class staticClass {
+    static String str = "Static class";
+
+    public static class staticCl {
+        public void output(){
+            System.out.println("Static class method");
+        }
+    }
+
+}
---
+++ Day3OOPinJava/2. StaticClass/staticClassImplement.java
@@ -0,0 +1,7 @@
+public class staticClassImplement {
+    // Like nested classes discusses in inner class
+    public static void main(String[] args) {
+        staticClass.staticCl sta = new staticClass.staticCl();
+        sta.output();   
+    }
+}
