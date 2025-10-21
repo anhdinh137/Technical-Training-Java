ae7948da01ffa8125eac13ad9a572d56c5fe0079
anhdinh137 <anh.dinh@hitachids.com>
2025-10-21 14:38:13 +0700

    TTJ-53 map interface

--- Day4Interfaces&Collections/4. MapInterface/MapInterface.java
+++ Day4Interfaces&Collections/4. MapInterface/MapInterface.java
@@ -1,3 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapInterface {
-    
+    // Part of the java.util package, represent a collection of key-value pair
+    // Key are unique, values can be dupes
+    // Hashmap, Linkedlistmap allow one null key, treemap does not
+    public static void main(String[] args) {
+        // Can also use LinkedHashMap or TreeMap
+        Map<Integer, String> map = new HashMap<>();
+
+        // Adding key-value pair to the map
+        map.put(1, "abc");
+        map.put(2,"def");
+        map.put(3, "ghy");
+        System.out.println(map);
+
+        // updating element
+        map.put(2, "win");
+        System.out.println(map);
+
+        // removing element
+        map.remove(2);
+        System.out.println(map);
+
+        // iterating through the map
+
+        for (Map.Entry<Integer, String> entry: map.entrySet()){
+            System.out.println(entry.getKey() + " "+ entry.getValue());
+        }
+    }
 }
