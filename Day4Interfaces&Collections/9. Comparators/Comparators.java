254222079f9282de2b6a5ff96747f54acdbbfecb
anhdinh137 <anh.dinh@hitachids.com>
2025-10-21 17:03:28 +0700

    TTJ-58 comparators

--- Day4Interfaces&Collections/9. Comparators/Comparators.java
+++ Day4Interfaces&Collections/9. Comparators/Comparators.java
@@ -1,3 +1,35 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 public class Comparators {
-    
+    // in java.util, used to sort object of user-defined class
+    // The comparator object compare 2 object
+    // if a < b  then negetive, a > b then positive, a == b then 0
+
+    static class sortById implements Comparator<Employee>{
+        // You can sort collection by more than 1 field
+        public int compare(Employee a, Employee b){
+            if (a.getId() != b.getId()) return a.getId()-b.getId();
+            else return a.getAge() - b.getAge();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Employee> list = new LinkedList<>();
+        list.add(new Employee(2, "a", 24));
+        list.add(new Employee(3, "b", 26));
+        list.add(new Employee(1, "c", 31));
+        list.add(new Employee(4, "d", 19));
+
+        for (Employee employee: list){
+            System.out.println(employee.getId() + " " + employee.getName() + " "+ employee.getAge());
+        }
+
+        Collections.sort(list, new sortById());
+
+        for (Employee employee: list){
+            System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getAge());
+        }
+    }
 }
---
+++ Day4Interfaces&Collections/9. Comparators/Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+    int id;
+    int age;
+    String name;
+    // simple employee class
+    public Employee(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age =  age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
