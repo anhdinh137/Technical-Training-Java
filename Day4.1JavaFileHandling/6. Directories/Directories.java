b23e2c203377b1cdc7f49b96ec26809c21818f28
anhdinh137 <anh.dinh@hitachids.com>
2025-10-22 15:49:42 +0700

    TTJ-68 directories

--- Day4.1JavaFileHandling/6. Directories/Directories.java
+++ Day4.1JavaFileHandling/6. Directories/Directories.java
@@ -1,3 +1,21 @@
+import java.io.File;
+
 public class Directories {
-    
+    public static void main(String[] args) {
+        String directoryName = "My_Learning";
+        
+        // current directory
+        String currentDirectory = System.getProperty("user.dir");
+        String directoryPath = currentDirectory + File.separator + directoryName;
+        File directory = new File(directoryPath);
+
+        // create directory
+        boolean directoryCreated = directory.mkdir();
+
+        if (directoryCreated) {
+            System.out.println("Directory created at: " + directoryPath);
+        } else {
+            System.out.println("Failed");
+        }
+    }
 }
